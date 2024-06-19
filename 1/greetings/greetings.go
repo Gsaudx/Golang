e1ae845c3c81ef1b19a65136802295199e1a6ae6
Guilherme Saud <gsaud1008@gmail.com>
2024-06-19 07:56:58 -0300

    docs: := syntax

--- 1/greetings/greetings.go
+++ 1/greetings/greetings.go
@@ -2,6 +2,11 @@ package greetings
 
 import "fmt"
 
+//? := is the shortcut to declaring and initializing a variable in go.
+//? Go uses the value on its right to determine the variable's type. But instead, we could have used:
+//! var message string
+//! message = fmt.Sprintf("Hi, %v. Welcome!", name)
+
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.

5e453cf62f3de4b51158a897b30f5e5aad8419b8
Guilherme Saud <gsaud1008@gmail.com>
2024-06-19 08:04:57 -0300

    feat: no name greeting validation

--- 1/greetings/greetings.go
+++ 1/greetings/greetings.go
@@ -1,15 +1,25 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("Empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// In a greeting message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
+	//!nil in this case means no errors
+}
 
 //? := is the shortcut to declaring and initializing a variable in go.
 //? Go uses the value on its right to determine the variable's type. But instead, we could have used:
 //! var message string
 //! message = fmt.Sprintf("Hi, %v. Welcome!", name)
-
-// Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
-}
